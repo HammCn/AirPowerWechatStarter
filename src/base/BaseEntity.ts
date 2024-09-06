e5859dc970f511b74c0ae8ae9f0dbf376fec7643
Hamm <majhamm@qq.com>
2024-09-06 19:10:01 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/base/BaseEntity.ts
+++ src/base/BaseEntity.ts
@@ -1,16 +1,13 @@
 import { AirEntity } from '../airpower/base/AirEntity'
-import { ToJson, ToModel } from '../airpower/decorator/Custom'
-import { AirDateTime } from '../airpower/helper/AirDateTime'
 
 export class BaseEntity extends AirEntity {
   /**
    * 创建时间
    */
-  @ToModel((val: number) => AirDateTime.formatFromSecond(val))
-  @ToJson(() => undefined) createTime!: number
+  createTime!: number
 
   /**
    * 修改时间
    */
-  @ToJson(() => undefined) updateTime!: number
+  updateTime!: number
 }
